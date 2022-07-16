a27b9c3624bc95103132b569cc9c8fa173285728
ported-pw <contact@ported.pw>
2022-07-16 21:16:26 +0200

    Allow setting S3 endpoint URL

--- influxdb-to-s3.sh
+++ influxdb-to-s3.sh
@@ -20,6 +20,12 @@ export DATABASE_META_DIR=${DATABASE_META_DIR:-/var/lib/influxdb/meta}
 export DATABASE_DATA_DIR=${DATABASE_DATA_DIR:-/var/lib/influxdb/data}
 export DATETIME=$(date "+%Y%m%d%H%M%S")
 
+if [ -z "${S3_ENDPOINT}" ]; then
+  AWS_ARGS=""
+else
+  AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+fi
+
 # Add this script to the crontab and start crond
 cron() {
   echo "Starting backup cron job with frequency '$1'"
@@ -49,18 +55,18 @@ backup() {
 
   # Push backup file to S3
   echo "Sending file to S3"
-  if aws s3 rm s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz; then
+  if aws $AWS_ARGS s3 rm s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz; then
     echo "Removed latest backup from S3"
   else
     echo "No latest backup exists in S3"
   fi
-  if aws s3 cp $BACKUP_ARCHIVE_PATH s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz; then
+  if aws $AWS_ARGS s3 $AWS_ARGS cp $BACKUP_ARCHIVE_PATH s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz; then
     echo "Backup file copied to s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz"
   else
     echo "Backup file failed to upload"
     exit 1
   fi
-  if aws s3api copy-object --copy-source ${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz --key ${S3_KEY_PREFIX}${DATETIME}.tgz --bucket $S3_BUCKET; then
+  if aws $AWS_ARGS s3api copy-object $AWS_ARGS --copy-source ${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz --key ${S3_KEY_PREFIX}${DATETIME}.tgz --bucket $S3_BUCKET; then
     echo "Backup file copied to s3://${S3_BUCKET}/${S3_KEY_PREFIX}${DATETIME}.tgz"
   else
     echo "Failed to create timestamped backup"
@@ -83,7 +89,7 @@ restore() {
   fi
   # Get backup file from S3
   echo "Downloading latest backup from S3"
-  if aws s3 cp s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz $BACKUP_ARCHIVE_PATH; then
+  if aws $AWS_ARGS s3 cp s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz $BACKUP_ARCHIVE_PATH; then
     echo "Downloaded"
   else
     echo "Failed to download latest backup"
